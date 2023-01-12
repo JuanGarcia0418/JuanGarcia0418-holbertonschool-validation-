53e66635e2fe97fa48706613908d6aa06420fee6
Juan Garcia <juanlds621@gmail.com>
2023-01-12 23:26:21 +0000

    add task 4

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-apt-get update && apt-get install -y git make wget build-essential && wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb && dpkg -i hugo_extended_0.109.0_linux-amd64.deb 
+apt-get update
+apt-get install -y hugo make wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb
+apt install ./hugo_0.84.0_Linux-64bit.deb
 make build
