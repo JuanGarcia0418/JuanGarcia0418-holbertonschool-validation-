4786fa065f858bef7ac3a755d99aaf29611cac16
Juan Garcia <juanlds621@gmail.com>
2023-01-17 15:45:23 +0000

    add task 4

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-apt update
-apt install -y git
-apt install -y hugo
-apt-get update && apt-get install make
-apt install -y curl
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
-apt install ./hugo.deb
+apt-get update
+apt-get install -y hugo make wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb
+apt install ./hugo_0.84.0_Linux-64bit.deb
 make build
